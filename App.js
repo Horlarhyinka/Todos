b77fbdcede3f559e78634767a1fcbf723c184a74
horlarhyinka <danijufarouq2003@gmail.com>
2022-04-04 16:44:32 +0100

    almost done

--- App.js
+++ App.js
@@ -1,24 +1,55 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+
+const url = 'mongodb+srv://Horlarh:1234@cluster0.syhy6.mongodb.net/Todos?retryWrites=true&w=majority';
 
 //setting view engine
 app.set("view engine",'ejs')
 
 //connecting to database
 
-
-const url = 'mongodb+srv://Horlarh:1234@cluster0.syhy6.mongodb.net/Todos?retryWrites=true&w=majority';
 mongoose.connect(url)
+
 .then(app.listen(3001,()=>{
     console.log("now listening to port 3001")
 })).catch(err=> console.log(err));
 
+//adding middlewares
+
+app.use(express.static('assets'));
+app.use(express.urlencoded({extended:true}))
+
+//creating schema
+
+
+const todoSchema = new mongoose.Schema({
+    todo:{ type:"string",required: true}
+})
+
+const Todo = mongoose.model("Todo",todoSchema);
+
 //setting up route
+const list = ['todo1','todo2']
 
 app.get('/todos',(req,res)=>{
-    const list = ['todo1','todo2']
-    res.render('index',{list})
+    Todo.find()
+    .then((result)=>{
+        res.render('index',{list:result});
+    })
+        
+    
+})
+app.post('/todos',(req,res)=>{
+    const newtodo = new Todo({
+        todo:req.body.todo
+    })
+    newtodo.save()
+    .then(()=>{
+        res.redirect('/todos')
+    }).catch((err)=>console.log(err))
+    
 })
 
 
